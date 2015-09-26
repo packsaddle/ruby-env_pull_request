59a997b54e13d7ab2a931e1a31e5f2e252f305d9
sanemat <o.gata.ken@gmail.com>
2015-09-26 15:35:58 +0900

    style(utils): fix declaration

--- lib/env_pull_request/utils.rb
+++ lib/env_pull_request/utils.rb
@@ -1,8 +1,9 @@
 module EnvPullRequest
   module Utils
-    def integral_string?(value)
-      value.is_a?(String) && /^\d+$/ =~ value ? true : false
-    end
-    module_function :integral_string?
+    module_function
+
+      def integral_string?(value)
+        value.is_a?(String) && /^\d+$/ =~ value ? true : false
+      end
   end
 end
