073c0c14f4d1e236e7f5f4595033e299c3d9244d
sanemat <o.gata.ken@gmail.com>
2015-09-26 14:54:35 +0900

    test(block): add test

--- test/test_ci.rb
+++ test/test_ci.rb
@@ -44,6 +44,34 @@ module EnvPullRequest
       end
     end
 
+    sub_test_case 'own ci' do
+      pull_id = 800
+      setup do
+        ENV['PULL_REQUEST_ID'] = pull_id.to_s
+      end
+
+      teardown do
+        ENV.delete 'PULL_REQUEST_ID'
+      end
+
+      sub_test_case 'with block' do
+        test '#pull_request?' do
+          assert do
+            EnvPullRequest.new do
+              ENV['PULL_REQUEST_ID'].to_i if ENV['PULL_REQUEST_ID']
+            end.pull_request? == true
+          end
+        end
+        test '#pull_request_id' do
+          assert do
+            EnvPullRequest.new do
+              ENV['PULL_REQUEST_ID'].to_i if ENV['PULL_REQUEST_ID']
+            end.pull_request_id == pull_id
+          end
+        end
+      end
+    end
+
     sub_test_case 'travis-ci' do
       sub_test_case 'pull request' do
         pull_id = 800
