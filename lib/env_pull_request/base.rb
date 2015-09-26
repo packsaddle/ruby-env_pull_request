3cdd71922488b272b80a818b43a23c61957f781d
sanemat <o.gata.ken@gmail.com>
2015-09-26 11:30:36 +0900

    style(fetch): fix by rubocop

--- lib/env_pull_request/base.rb
+++ lib/env_pull_request/base.rb
@@ -7,7 +7,8 @@ module EnvPullRequest
     end
 
     def fetch_pull_request_id
-      if ENV['TRAVIS_PULL_REQUEST'] && ENV['TRAVIS_PULL_REQUEST'].downcase != 'false'
+      if ENV['TRAVIS_PULL_REQUEST'] &&
+         ENV['TRAVIS_PULL_REQUEST'].downcase != 'false'
         ENV['TRAVIS_PULL_REQUEST'].to_i
       elsif ENV['CIRCLE_PR_NUMBER']
         ENV['CIRCLE_PR_NUMBER'].to_i
