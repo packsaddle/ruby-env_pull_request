391fe6b416c5855eab0c7c82cd939b57db7315d3
sanemat <o.gata.ken@gmail.com>
2015-09-26 15:46:44 +0900

    test(block): superior user definition

--- test/test_ci.rb
+++ test/test_ci.rb
@@ -77,6 +77,35 @@ module EnvPullRequest
             end.pull_request_id == pull_id
           end
         end
+
+        sub_test_case 'superior user definition' do
+          env_pull_id = 900
+          setup do
+            ENV['TRAVIS_PULL_REQUEST'] = env_pull_id.to_s
+          end
+
+          teardown do
+            ENV.delete 'TRAVIS_PULL_REQUEST'
+          end
+          test '#pull_request?' do
+            assert do
+              EnvPullRequest.new do
+                if Utils.integral_string? ENV['PULL_REQUEST_ID']
+                  ENV['PULL_REQUEST_ID'].to_i
+                end
+              end.pull_request? == true
+            end
+          end
+          test '#pull_request_id' do
+            assert do
+              EnvPullRequest.new do
+                if Utils.integral_string? ENV['PULL_REQUEST_ID']
+                  ENV['PULL_REQUEST_ID'].to_i
+                end
+              end.pull_request_id == pull_id
+            end
+          end
+        end
       end
     end
 
