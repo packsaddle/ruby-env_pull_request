d70d122472895f6954aee06b194fb19dc6518f99
sanemat <o.gata.ken@gmail.com>
2015-09-26 12:31:03 +0900

    chore(util): check string

--- test/test_utils.rb
+++ test/test_utils.rb
@@ -6,10 +6,37 @@ module EnvPullRequest
       include Utils
     end.new
 
-    sub_test_case 'integral' do
+    sub_test_case 'not integral' do
       test 'nil' do
         assert do
           target.integral_string?(nil) == false
+        end
+      end
+      test 'empty string' do
+        assert do
+          target.integral_string?('') == false
+        end
+      end
+      test 'integer' do
+        assert do
+          target.integral_string?(1) == false
+        end
+      end
+      test 'decimal string' do
+        assert do
+          target.integral_string?('1.1') == false
+        end
+      end
+    end
+    sub_test_case 'integral' do
+      test '1' do
+        assert do
+          target.integral_string?('1') == true
+        end
+      end
+      test '10000000000000000' do
+        assert do
+          target.integral_string?('10000000000000000') == true
         end
       end
     end
