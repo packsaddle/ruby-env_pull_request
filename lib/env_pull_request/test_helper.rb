a9a02cfcb31c2e13486aafbe6e963da7cc8e64d8
sanemat <o.gata.ken@gmail.com>
2015-09-27 17:48:41 +0900

    docs(helper): add yard

--- lib/env_pull_request/test_helper.rb
+++ lib/env_pull_request/test_helper.rb
@@ -1,6 +1,21 @@
 module EnvPullRequest
+  # Test helper for pull request with environment variables
   module TestHelper
+    # Stash original environment variables for pull request.
+    # And delete for testing.
+    #
+    # @example with test-unit
+    #   class TestExample < Test::Unit::TestCase
+    #     extend TestHelper
+    #
+    #     def self.startup
+    #       stash_env_pull_request
+    #     end
+    #   end
+    #
+    # @return [void]
     def stash_env_pull_request
+      # NOTE: stash and restore only if exists
       @original_travis_pull_request = ENV['TRAVIS_PULL_REQUEST']
       @original_circle_pr_number = ENV['CIRCLE_PR_NUMBER']
       @original_ghprb_pull_id = ENV['ghprbPullId']
@@ -9,6 +24,18 @@ module EnvPullRequest
       ENV.delete 'ghprbPullId'
     end
 
+    # Restore original environment variables for pull request.
+    #
+    # @example with test-unit
+    #   class TestExample < Test::Unit::TestCase
+    #     extend TestHelper
+    #
+    #     def self.shutdown
+    #       restore_env_pull_request
+    #     end
+    #   end
+    #
+    # @return [void]
     def restore_env_pull_request
       ENV['TRAVIS_PULL_REQUEST'] = @original_travis_pull_request
       ENV['CIRCLE_PR_NUMBER'] = @original_circle_pr_number
