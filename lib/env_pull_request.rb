75c5e5ebc9b6b35c8bc24057182fb272e4c9c446
sanemat <o.gata.ken@gmail.com>
2015-09-27 17:07:38 +0900

    docs(app): update

--- lib/env_pull_request.rb
+++ lib/env_pull_request.rb
@@ -3,18 +3,27 @@ require 'natural_number_string'
 require 'env_pull_request/base'
 require 'env_pull_request/version'
 
+# Build pull request information from environment variables
 module EnvPullRequest
-  # @return [Base]
-  # @yield user defined block
-  # @example without block
-  #   env_pull = EnvPullRequest.new
-  # @example with block
-  #   env_pull =
-  #     EnvPullRequest.new do
-  #       if NaturalNumberString.positive_integer_string? ENV['PULL_REQUEST_ID']
-  #         ENV['PULL_REQUEST_ID'].to_i
+  # Build pull request information object from environment variables
+  #
+  # @overload self.new
+  #   @example without user defined block
+  #     env_pull = EnvPullRequest.new
+  #
+  #   @return [Base]
+  #
+  # @overload self.new(&block)
+  #   @example with user defined block
+  #     env_pull =
+  #       EnvPullRequest.new do
+  #         if NaturalNumberString.positive_integer_string? ENV['PULL_REQUEST_ID']
+  #           ENV['PULL_REQUEST_ID'].to_i
+  #         end
   #       end
-  #     end
+  #
+  #   @yield user defined block
+  #   @return [Base]
   def self.new(&block)
     Base.new(&block)
   end
