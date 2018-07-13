9a9e9d11fc3edf8a030e84f5abe73738c88c138d
Kyosuke Takayama <loiseau@gmail.com>
2018-07-13 15:49:22 +0900

    feat(ci): support bitrise

--- test/test_external_ci.rb
+++ test/test_external_ci.rb
@@ -79,6 +79,28 @@ module EnvPullRequest
       end
     end
 
+    sub_test_case 'bitrise' do
+      pull_id = 800
+      setup do
+        ENV['BITRISE_PULL_REQUEST'] = pull_id.to_s
+      end
+
+      teardown do
+        ENV.delete 'BITRISE_PULL_REQUEST'
+      end
+
+      test '#pull_request?' do
+        assert do
+          EnvPullRequest.new.pull_request? == true
+        end
+      end
+      test '#pull_request_id' do
+        assert do
+          EnvPullRequest.new.pull_request_id == pull_id
+        end
+      end
+    end
+
     sub_test_case 'Jenkins GitHub pull request builder plugin' do
       pull_id = 800
       setup do
